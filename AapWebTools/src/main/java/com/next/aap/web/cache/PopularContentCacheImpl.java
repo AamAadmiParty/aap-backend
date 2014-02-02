64babfe56c027ad4e647031d735a067d6e5e3083
Ravi Sharma <ping2ravi@gmail.com>
2014-02-02 02:37:53 +0000

    added @Component

--- AapWebTools/src/main/java/com/next/aap/web/cache/PopularContentCacheImpl.java
+++ AapWebTools/src/main/java/com/next/aap/web/cache/PopularContentCacheImpl.java
@@ -2,19 +2,27 @@ package com.next.aap.web.cache;
 
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
 
 import com.next.aap.core.service.AapService;
 import com.next.aap.web.dto.BlogDto;
 import com.next.aap.web.dto.NewsDto;
 import com.next.aap.web.dto.VideoDto;
 
+@Component
 public class PopularContentCacheImpl implements PopularContentCache {
 
 	@Autowired
 	private AapService aapService;
 	
+	@PostConstruct
+	public void init(){
+		refreshPopularContent();
+	}
 	//Connect to Google Analytics and get analytics every hour
 	@Scheduled(cron = "01 01 * * * *")
 	public void refreshPopularContent() {
